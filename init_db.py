ad77fe0fc7f89a9fe4309ac95d5204282c2caf38
jwolf <jmwolf@ucla.edu>
2023-06-26 13:51:32 -0500

    Add init_db script
    This script can be used to initialize the `fsync_db` database.

---
+++ init_db.py
@@ -0,0 +1,44 @@
+import os
+
+import psycopg2
+
+conn = psycopg2.connect(
+    host="localhost",
+    database="fsync_db",
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+)
+
+cur = conn.cursor()
+
+# Create the User tokens table.
+cur.execute("DROP TABLE IF EXISTS user_tokens;")
+cur.execute(
+    """CREATE TABLE user_tokens (
+        fibit_id varchar (10) PRIMARY KEY,
+        fitbit_access_token varchar (400) NOT NULL,
+        fitbit_refresh_token varchar (100) NOT NULL,
+        strava_access_token varchar (400) NOT NULL,
+        strava_refresh_token varchar (100) NOT NULL,
+        date_added date DEFAULT CURRENT_TIMESTAMP,
+        fitbit_date_refreshed date DEFAULT CURRENT_TIMESTAMP,
+        strava_data_refreshed date DEFAULT CURRENT_TIMESTAMP);"""
+)
+
+# Create the User activity table.
+cur.execute("DROP TABLE IF EXISTS user_activity;")
+cur.execute(
+    """CREATE TABLE user_activity (
+        fitbit_id varchar (10),
+        fitbit_latest_activity_date date NOT NULL DEFAULT date '2000-01-01',
+        strava_latest_activity_date date NOT NULL DEFAULT date '2000-01-01',
+        CONSTRAINT fk_fitbit_id
+            FOREIGN KEY(fitbit_id)
+                REFERENCES user_tokens(fitbit_id)
+                ON DELETE CASCADE
+    );"""
+)
+
+conn.commit()
+cur.close()
+conn.close()
