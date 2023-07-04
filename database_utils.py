16e48647e018789bf8d0e89055d8a21c5d4cd927
jwolf <jmwolf@ucla.edu>
2023-07-04 11:43:04 -0500

    Update database connections
    Switch to using a connection pool for distributing database connections.

--- database_utils.py
+++ database_utils.py
@@ -3,16 +3,20 @@ import os
 
 import psycopg2
 
+db_connection_pool = psycopg2.ThreadedConnectionPool(
+    minconn=1,
+    maxconn=15,
+    host=os.getenv("DB_HOST"),
+    database=os.getenv("DB_NAME"),
+    user=os.getenv("DB_USERNAME"),
+    password=os.getenv("DB_PASSWORD"),
+)
+
 
 @contextmanager
 def get_db_connection():
-    conn = psycopg2.connect(
-        host="localhost",
-        database="fsync_db",
-        user=os.getenv("DB_USERNAME"),
-        password=os.getenv("DB_PASSWORD"),
-    )
+    conn = db_connection_pool.getconn()
     try:
         yield conn
     finally:
-        conn.close()
+        db_connection_pool.putconn(conn)
