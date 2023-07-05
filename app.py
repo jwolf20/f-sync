ce608d321287776e4761d9784b4aa39ac8a71be7
jwolf <jmwolf@ucla.edu>
2023-07-05 11:24:09 -0500

    Added Oauth functionality
    It should now be possible for new users to register with the application.

--- app.py
+++ app.py
@@ -2,11 +2,23 @@ import datetime
 import io
 import json
 from logging.config import dictConfig
+import os
+import secrets
+from urllib.parse import urlencode
 
-from flask import Flask, render_template, request
+from flask import (
+    abort,
+    flash,
+    Flask,
+    redirect,
+    render_template,
+    request,
+    session,
+    url_for,
+)
 
 from celery_utils import get_celery_app_instance
-from database_utils import get_db_connection
+from database_utils import get_db_connection, database_create_user, database_user_check
 from fitbit_api import *
 from strava_api import *
 
@@ -164,6 +176,206 @@ def webhook_link():
             return "Success", 204
         else:
             return "Failure", 404
+
+
+# OAUTH section
+# This section contains the code necessary to interact with Oauth allowing users to register for the application.
+@app.rout("/auth/success")
+def auth_success():
+    return render_template("auth_success.html")
+
+
+def generate_pkce_code():
+    random_token = secrets.token_urlsafe(90)
+    sha256_hash = hashlib.sha256(random_token.encode()).digest()
+    pkce_code = base64.urlsafe_b64encode(sha256_hash).rstrip(b"=").decode()
+    return pkce_code
+
+
+@app.route("/auth/fitbit")
+def fitbit_oauth():
+    session["pkce_code"] = generate_pkce_code()
+    session["oauth2_fitbit_state"] = secrets.token_urlsafe(20)
+    param_string = urlencode(
+        {
+            "response_type": "code",
+            "client_id": os.getenv("FITBIT_CLIENT_ID"),
+            "scope": "activity",
+            "code_challenge": session["pkce_code"],
+            "code_challenge_method": "S256",
+            "state": session["oauth2_fitbit_state"],
+            "redirect_uri": os.getenv("FITBIT_REDIRECT_URI"),
+        }
+    )
+    authorization_address = f"https://www.fitbit.com/oauth2/authorize?{param_string}"
+    return redirect(authorization_address)
+
+
+@app.route("/auth_verify/fitbit")
+def fitbit_oauth_verify():
+    # Verify the redirect has required information
+    if "error" in request.args:
+        for k, v in request.args.items():
+            if k.startswith("error"):
+                flash(f"{k}: {v}")
+        return redirect(url_for("index"))
+
+    if request.args["state"] != session.get("oauth2_fitbit_state"):
+        abort(401)
+
+    if session.get("pkce_code") is None:
+        abort(401)
+
+    if "code" not in request.args:
+        abort(401)
+
+    # Submit request for tokens
+    ## Construct request parameters
+    params = {
+        "client_id": os.getenv("FITBIT_CLIENT_ID"),
+        "grant_type": "authorization_code",
+        "redirect_uri": os.getenv("FITBIT_REDIRECT_URI"),
+        "code": request.args.get("code"),
+        "code_verifier": session["pkce_code"],
+    }
+
+    basic_token = base64.urlsafe_b64encode(
+        f"{os.getenv('FITBIT_CLIENT_ID')}:{os.getenv('FITBIT_CLIENT_SECRET')}".encode()
+    ).decode()
+
+    ## Construct request headers
+    headers = {
+        "Authorization": f"Basic {basic_token}",
+        "Content-Type": "application/x-www-form-urlencoded",
+    }
+
+    ## Submit request
+    response = requests.post(
+        url="https://api.fitbit.com/oauth2/token", params=params, headers=headers
+    )
+
+    # Verify request is successful
+    if response.status_code != 200:
+        abort(401)
+
+    access_token = response.json().get("access_token")
+    if not access_token:
+        abort(401)
+
+    refresh_token = response.json().get("refresh_token")
+    if not refresh_token:
+        abort(401)
+
+    fitbit_id = response.json().get("user_id")
+    if not fitbit_id:
+        abort(401)
+
+    # TODO: Check that user provided appropriate scope access
+    app.logger.info(f"{fitbit_id=}, scope: {response.json().get('scope')}")
+
+    # Add user to database
+    user_added = database_create_user(fitbit_id=fitbit_id)
+    if user_added:
+        app.logger.info(f"Added New User to the database with{fitbit_id=}")
+    else:
+        app.logger.warning(
+            f"User with {fitbit_id=} has re-registered for the application."
+        )
+
+    update_fitbit_tokens(response.json(), fitbit_id=fitbit_id)
+
+    return redirect(url_for("strava_oauth", fitbit_id=fitbit_id))
+
+
+@app.route("/auth/strava/<fitbit_id>")
+def strava_oauth(fitbit_id):
+    # Used to check that the request is coming from a redirect
+    if session.get("oauth2_fitbit_state") is None:
+        abort(401)
+
+    # Verify user is valid
+    if not database_user_check(fitbit_id=fitbit_id):
+        abort(401)
+
+    # Generate a new state token
+    session["oauth2_strava_state"] = f"{fitbit_id}_{secrets.token_urlsafe(20)}"
+    param_string = urlencode(
+        {
+            "client_id": os.getenv("STRAVA_CLIENT_ID"),
+            "scope": "activity:write,activity:read",
+            "response_type": "code",
+            "approval_prompt": "auto",
+            "state": session["oauth2_strava_state"],
+            "redirect_uri": os.getenv("STRAVA_REDIRECT_URI"),
+        }
+    )
+    authorization_address = f"https://www.strava.com/oauth/authorize?{param_string}"
+    return redirect(authorization_address)
+
+
+@app.route("/auth_verify/strava")
+def strava_oauth_verify():
+    # Verify the redirect has required information
+    if "error" in request.args:
+        for k, v in request.args.items():
+            if k.startswith("error"):
+                flash(f"{k}: {v}")
+        return redirect(url_for("index"))
+
+    if request.args["state"] != session.get("oauth2_strava_state"):
+        abort(401)
+
+    if "code" not in request.args:
+        abort(401)
+
+    fitbit_id, *_ = session.get("oauth2_strava_state").split("_")
+    # Submit request for tokens
+    ## Construct request parameters
+    params = {
+        "client_id": os.getenv("STRAVA_CLIENT_ID"),
+        "client_secret": os.getenv("STRAVA_CLIENT_SECRET"),
+        "grant_type": "authorization_code",
+        "code": request.args.get("code"),
+    }
+
+    ## Submit request
+    response = requests.post(
+        url="https://www.strava.com/api/v3/oauth/token", params=params
+    )
+
+    # Verify request is successful
+    if response.status_code != 200:
+        abort(401)
+
+    access_token = response.json().get("access_token")
+    if not access_token:
+        abort(401)
+
+    refresh_token = response.json().get("refresh_token")
+    if not refresh_token:
+        abort(401)
+
+    # Add tokens to the database
+    update_strava_tokens(response.json(), fitbit_id=fitbit_id)
+
+    # Add user to webhook endpoint subscription
+    response = fitbit_webhook_subscribe(subscriber_id=1, fitbit_id=fitbit_id)
+
+    if response.status_code == 200:
+        app.logger.warning(
+            f"User {fitbit_id=} already had an active webhook subscription."
+        )
+    elif response.status_code == 201:
+        app.logger.info(
+            f"Successfully created webhook subscription for user {fitbit_id=}!"
+        )
+    else:
+        app.logger.error(
+            f"Something went wrong attempting to create a webhook subscription for user {fitbit_id=}!"
+        )
+        abort(401)
+
+    return redirect(url_for("auth_success"))
 
 
 if __name__ == "__main__":
